05868c71c86993433356f4fd920ab274963517fa
Martin Buberl <hello@martinbuberl.com>
2024-10-22 18:59:42 +0200

    Switch to ESM

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,9 @@
-module.exports.config = {
+// https://www.11ty.dev/docs/config/
+
+export default async function(eleventyConfig) {
+};
+
+export const config = {
   dir: {
     input: "src", // default: "."
     output: "dist", // default: "_site"
