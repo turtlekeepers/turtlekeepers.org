ecaa43192ba995621140b827179f25cf5f7ae5fb
Martin Buberl <hello@martinbuberl.com>
2024-10-22 16:51:38 +0200

    Add basic site

---
+++ eleventy.config.js
@@ -0,0 +1,7 @@
+module.exports.config = {
+  dir: {
+    input: "src", // default: "."
+    output: "dist", // default: "_site"
+    layouts: "_layouts" // default: "_layouts" (`input` relative)
+  }
+};
