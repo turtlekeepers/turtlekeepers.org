6e66669b2a64fdeb1c2f31f9b2f898ba4a5d6052
Martin Buberl <hello@martinbuberl.com>
2024-10-23 16:35:11 +0200

    Removed as default

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,6 +7,5 @@ export const config = {
   dir: {
     input: "src", // default: "."
     output: "dist", // default: "_site"
-    layouts: "_layouts" // default: "_layouts" (`input` relative)
   }
 };
