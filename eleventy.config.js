ce23d2c057b00bf4aa7be2faa6fe54379bb34296
Martin Buberl <hello@martinbuberl.com>
2024-10-24 16:10:53 +0200

    Move CNAME to root

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,7 +5,7 @@ const outputDir = "dist"; // default: "_site"
 const layoutsDir = "_layouts"; // default: "_layouts" (`input` relative)
 
 export default async function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy(`${inputDir}/CNAME`);
+  //eleventyConfig.addPassthroughCopy(`${inputDir}/CNAME`);
 };
 
 export const config = {
