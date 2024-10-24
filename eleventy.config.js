8b7decc44a807df1565e7adda0fbae1dfb334072
Martin Buberl <hello@martinbuberl.com>
2024-10-24 15:28:04 +0200

    Copy CNAME to output directory

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,17 @@
 // https://www.11ty.dev/docs/config/
 
+const inputDir = "src"; // default: "."
+const outputDir = "dist"; // default: "_site"
+const layoutsDir = "_layouts"; // default: "_layouts" (`input` relative)
+
 export default async function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy(`${inputDir}/CNAME`);
 };
 
 export const config = {
   dir: {
-    input: "src", // default: "."
-    output: "dist", // default: "_site"
-    layouts: "_layouts" // default: "_layouts" (`input` relative)
+    input: inputDir,
+    output: outputDir,
+    layouts: layoutsDir
   }
 };
