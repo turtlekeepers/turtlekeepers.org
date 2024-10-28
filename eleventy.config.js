29a4880c725559c0f94b33f03f2125b14e9dcd71
Martin Buberl <hello@martinbuberl.com>
2024-10-28 20:45:48 +0100

    Add SASS pipeline

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,6 @@
 // https://www.11ty.dev/docs/config/
+import * as sass from "sass";
+import path from "node:path";
 
 const inputDir = "src"; // default: "."
 const outputDir = "dist"; // default: "_site"
@@ -6,6 +8,34 @@ const layoutsDir = "_layouts"; // default: "_layouts" (`input` relative)
 
 export default async function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy(`${inputDir}/robots.txt`);
+
+  // SASS pipeline
+  eleventyConfig.addTemplateFormats("scss");
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compileOptions: {
+      permalink: function (inputContent, inputPath) {
+        let re = new RegExp(String.raw`\/${inputDir}\/_`, "g");
+
+        return inputPath
+          .replace(re, "/") // changes e.g. "/src/_assets" to "/assets"
+          .replace(/\.scss$/, ".css");
+      }
+    },
+    compile: function(inputContent, inputPath) {
+      // skip partials (e.g. _normalize.scss)
+      if (path.parse(inputPath).base.startsWith("_")) {
+        return;
+      }
+
+      return (data) => {
+        let result = sass.compile(inputPath, {
+          style: "compressed"
+        });
+        return result.css.toString();
+      }
+    }
+  });
 };
 
 export const config = {
